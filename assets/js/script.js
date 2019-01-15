3cbcc98b734242622c798d26a937f61bacbab1f7
DavidHamelin <hamelindavid@hotmail.fr>
2019-01-15 10:01:16 +0100

    js Ex 01 part 03

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+﻿function borderOver() {
+    document.getElementById("image1").style.border = "3px solid red";
+}
+function borderOut() {
+    document.getElementById("image1").style.border = "none";
+}
